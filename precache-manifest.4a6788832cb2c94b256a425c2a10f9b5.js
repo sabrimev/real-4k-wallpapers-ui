bfb07e2a1afa5e6839511ac9d8b5bfbb4ee8c76d
Sabri Meviş <sabrimev@gmail.com>
2020-01-11 12:42:27 +0300

    Updates

---
+++ precache-manifest.4a6788832cb2c94b256a425c2a10f9b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c96cf8b7e2752b531c5a0b7202bd453",
+    "url": "/real-4k-wallpapers-ui/index.html"
+  },
+  {
+    "revision": "4afc396e82a655e9cb5b",
+    "url": "/real-4k-wallpapers-ui/static/css/main.d3eb5f57.chunk.css"
+  },
+  {
+    "revision": "fe9e67a82e25bc17afc5",
+    "url": "/real-4k-wallpapers-ui/static/js/2.7a94d7cc.chunk.js"
+  },
+  {
+    "revision": "3d9880ee0da2a100dc4a6223e0aed39a",
+    "url": "/real-4k-wallpapers-ui/static/js/2.7a94d7cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4afc396e82a655e9cb5b",
+    "url": "/real-4k-wallpapers-ui/static/js/main.3e619b11.chunk.js"
+  },
+  {
+    "revision": "fef25f6c8ec615ac83f4",
+    "url": "/real-4k-wallpapers-ui/static/js/runtime-main.e5e142b4.js"
+  }
+]);
